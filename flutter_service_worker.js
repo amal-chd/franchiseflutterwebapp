93a01c101a068cebaaf25b13def0670217bf8f0e
Amal Chand <amalchand@Amals-Mac-mini.lan>
2025-12-28 23:35:05 +0530

    Deploy: 2025-12-28 23:35:05

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8919064883ae85b2b0ee99dd7b26e36b",
+const RESOURCES = {"flutter_bootstrap.js": "2bc357c1ca6953fac2b1374322b98c68",
 "version.json": "f26ef0f35959a4727e27377b93c5244d",
 "index.html": "91b4de5f80b808ac77e1c612d04ef884",
 "/": "91b4de5f80b808ac77e1c612d04ef884",
-"main.dart.js": "063f5af038f98d09ac649859040afc33",
+"main.dart.js": "b3817fa7fcbae29833b3700b83f6bc26",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "4fa8971472a861c508a1fbea4709cf8c",
 "icons/Icon-192.png": "4fa8971472a861c508a1fbea4709cf8c",
