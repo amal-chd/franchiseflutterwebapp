4d77a42b73a992bc2686e57d737ea1badd740d03
Amal Chand <amalchand@Amals-Mac-mini.lan>
2025-12-29 00:13:05 +0530

    Deploy: 2025-12-29 00:13:05

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "167646258"
+    serviceWorkerVersion: "4025836301"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "633e3c2f116ff369fd239d2dc0a854de",
+const RESOURCES = {"flutter_bootstrap.js": "8f13eddd1a0e8237a394a722477e3949",
 "version.json": "f26ef0f35959a4727e27377b93c5244d",
 "index.html": "91b4de5f80b808ac77e1c612d04ef884",
 "/": "91b4de5f80b808ac77e1c612d04ef884",
-"main.dart.js": "0e302b830a382a201474c32b47165305",
+"main.dart.js": "76b1fec891898d302eb7edf6e612095e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "4fa8971472a861c508a1fbea4709cf8c",
 "icons/Icon-192.png": "4fa8971472a861c508a1fbea4709cf8c",
@@ -24,7 +24,7 @@ const RESOURCES = {"flutter_bootstrap.js": "633e3c2f116ff369fd239d2dc0a854de",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "140e122231a741896945bbacf8aada8b",
-"assets/fonts/MaterialIcons-Regular.otf": "94d82df619850ca19f733f408fcec92f",
+"assets/fonts/MaterialIcons-Regular.otf": "24b5206c45d33125a2cd0f4fd9d8145a",
 "assets/assets/images/login_logo.png": "e3d602f368d8fa4b14d76666523d139b",
 "assets/assets/images/logo_text.png": "a0751da2ccf0c17aa286b97be6bf3d55",
 "assets/assets/images/header_logo_new.png": "a0751da2ccf0c17aa286b97be6bf3d55",
