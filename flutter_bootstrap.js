0d6556c431fb601183bda1539b005bf80be52f6e
Amal Chand <amalchand@Amals-Mac-mini.lan>
2025-12-28 22:08:03 +0530

    Deploy latest build: login logo update, chat enhancements, post display fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2602323863"
+    serviceWorkerVersion: "2636772645"
   }
 });
